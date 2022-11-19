65e97d70ceb68efdf225623b39ed168e5a824f46
p3jit <prithi.das.007@gmail.com>
2022-11-20 01:23:52 +0530

    added video send

--- src/components/rightnav/outgoingchat/OutgoingChat.js
+++ src/components/rightnav/outgoingchat/OutgoingChat.js
@@ -7,10 +7,13 @@ function OutgoingChat({msg}) {
   return (
     <div className='self-end'>
       <div className='flex gap-5'>
-        <div className='flex flex-col gap-5'>
+        <div className='flex flex-col gap-2'>
           <div className='p-4 bg-sky-800 w-fit rounded-lg self-end'>{msg.text}</div>
           <div className=' flex justify-center hover:cursor-pointer'>
             {msg.img && <img src={ msg.img } loading="lazy" alt="chat-img" className='max-w-[400px] max-h-[300px] object-cover p-2 bg-sky-800'/>}
+          </div>
+          <div className=' flex justify-center hover:cursor-pointer'>
+            {msg.video && <video width={300} height={200} controls src={ msg.video } loading="lazy" alt="chat-video" className='p-2 bg-sky-800'/>}
           </div>
         </div>
         <img src={ currentUser.photoURL || `./icons/user.png`} loading="lazy" alt="chat-img" className='w-12 h-12 object-cover rounded-full'/>
--- src/components/rightnav/rightinputcontainer/RightInputContainer.js
+++ src/components/rightnav/rightinputcontainer/RightInputContainer.js
@@ -7,11 +7,14 @@ import { v4 as uuid } from 'uuid'
 import { AuthContext } from '../../../context/AuthContext';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { BiTrash } from "react-icons/bi";
+import { BiVideoPlus } from "react-icons/bi";
 
 function RightInputContainer() {
   const [text,setText] = useState("");
   const [img,setImg] = useState(null);
+  const [video,setVideo] = useState(null);
   const imgRef = useRef();
+  const videoRef = useRef();
   const textRef =useRef();
 
   const {data} = useContext(ChatContext);
@@ -19,6 +22,10 @@ function RightInputContainer() {
 
   const handleImgClick = () => {
     imgRef.current.click();
+  };
+
+  const handleVideoClick = () => {
+    videoRef.current.click();
   };
 
   const handleSend = async () => {
@@ -37,6 +44,23 @@ function RightInputContainer() {
           senderId: currentUser.uid,
           date: Timestamp.now(),
           img: downloadURL
+        }),
+      });
+    }
+    else if (video) { 
+      const storageRef = ref(storage, uuid());
+
+      const uploadSnapshot = await uploadBytes(storageRef,video);
+
+      const downloadURL = await getDownloadURL(uploadSnapshot.ref);
+
+      await updateDoc(doc(db,"chats",data.chatId) , {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+          video: downloadURL
         }),
       });
     }
@@ -73,6 +97,10 @@ function RightInputContainer() {
     setImg(null);
   }
 
+  const handleDeleteVideo = () => {
+    setVideo(null);
+  }
+
   return (
     <div className='flex w-full justify-between mb-1 flex-col'>
         {img && 
@@ -81,12 +109,22 @@ function RightInputContainer() {
             <button className='bg-zinc-500 p-2 rounded-full' onClick={handleDeleteImage}><BiTrash/></button>
           </div>
         }
+        {video && 
+          <div className='flex bg-zinc-800 py-2 pl-5 gap-2 items-center'>
+            <h1>{video.name}</h1>
+            <button className='bg-zinc-500 p-2 rounded-full' onClick={handleDeleteVideo}><BiTrash/></button>
+          </div>
+        }
         <div className='flex'>
           <input className='outline-none rounded-md w-full text-black py-2 px-3 my-3 mx-4' onKeyUp={e=>{setText(e.target.value)}} ref={textRef}/>
           <div className='flex items-center gap-3 mr-5'>
               <input type="file" style={{display: 'none'}} ref={imgRef} onChange={e=>setImg(e.target.files[0])} accept=".png,.jpg,.jpeg"/>
+              <input type="file" style={{display: 'none'}} ref={videoRef} onChange={e=>setVideo(e.target.files[0])} accept=".mkv,.mp4,.webm"/>
               <button className='text-2xl flex relative' onClick={handleImgClick}>
                 <BiImageAdd className='text-3xl'/>
+              </button>
+              <button className='text-2xl flex relative' onClick={handleVideoClick}>
+                <BiVideoPlus className='text-3xl'/>
               </button>
               <button className='px-5 py-2 bg-zinc-800 rounded-lg' onClick={handleSend}>Send</button>
           </div>
